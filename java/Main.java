01d18c3b2f4d2ca0bb97f3c0e17f3ac2b701801f
won3956 <won3956@gmail.com>
2026-02-02 18:15:50 +0900

    디렉토리 위치 변경 등

---
+++ java/Main.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        Stack<Integer> stack = new Stack<>();
+}
--- java/Main12789.java
+++ java/Main12789.java
@@ -4,8 +4,40 @@ import java.util.*;
 public class Main12789 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         StringBuilder sb;
+        int ent = 0;
 
+        int N = Integer.parseInt(br.readLine());
+        int arr[] = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            arr[i] = x;
+        }
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < N; i++) {
+            if (arr[i] == (ent + 1)) {
+                ent++;
+            } else {
+                stack.add(arr[i]);
+            }
+            while(!stack.empty() && stack.peek() == ent + 1) {
+                stack.pop();
+                ent++;
+            }
+        }
+        for (int i = 0; i < N - ent; i++) {
+            int temp = stack.pop();
+            if (temp == (ent + 1)) {
+                ent++;
+            } else {
+                System.out.print("Sad");
+                return;
+            }
+        }
+        System.out.print("Nice");
+        return;
     }
 }
