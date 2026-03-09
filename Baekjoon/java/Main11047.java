91a8d18270d10e62a78d06bc24dcfd9a85d0e79c
won3956 <won3956@gmail.com>
2026-03-09 21:27:23 +0900

    11

---
+++ Baekjoon/java/Main11047.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main11047 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb;
+        int point = 1;
+        int count = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        for (int i = N - 1; i > -1; i--) {
+            if (arr[i] <= K) {
+                point = i;
+                count += K / arr[i];
+                K = K % arr[i];
+            }
+        }
+
+        System.out.print(count);
+    }
+}
