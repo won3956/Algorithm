bf16a44d918368c60de449a40889a094acc96c09
wonr3956 <wonr3956@naver.com>
2026-04-22 22:52:01 +0900

    병합

---
+++ Baekjoon/java/Main18258.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main18258 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Queue<Integer> que = new LinkedList<>();
+    }
+}
--- Baekjoon/java/Main28278.java
+++ Baekjoon/java/Main28278.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-
+// 스택
 public class Main28278 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
