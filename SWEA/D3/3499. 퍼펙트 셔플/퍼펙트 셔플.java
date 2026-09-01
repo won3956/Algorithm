6fb29c3f6746f3b0a5ea07e6fc0bd86cc6b1ccd8
JiwonMoon <132432108+won3956@users.noreply.github.com>
2026-09-01 14:14:00 +0900

    [D3] Title: 퍼펙트 셔플, Time: 104 ms, Memory: 31,232 KB -BaekjoonHub

--- SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.java
+++ SWEA/D3/3499. 퍼펙트 셔플/퍼펙트 셔플.java
@@ -9,40 +9,31 @@ import java.util.*;
  */
 
 public class Solution {
-	static Deque<String> que1;
-	static Deque<String> que2;
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		for (int test_case = 1; test_case <= T; test_case++) {
-			StringBuilder sb = new StringBuilder();
-			que1 = new ArrayDeque<>();
-			que2 = new ArrayDeque<>();
 			int N = Integer.parseInt(br.readLine());
+			StringBuilder sb = new StringBuilder();
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			if(N%2==0) {
-				for(int i = 0; i < N/2; i++) {
-					que1.offer(st.nextToken());
-				}
-				for(int i = N/2; i < N; i++) {
-					que2.offer(st.nextToken());
-				}
-			}else {
-				for(int i = 0; i < N/2 + 1; i++) {
-					que1.offer(st.nextToken());
-				}
-				for(int i = N/2 + 1; i < N; i++) {
-					que2.offer(st.nextToken());
-				}
-			}
+			String[] cards = new String[N];
 			sb.append("#").append(test_case).append(" ");
-			for(int i = 0; i < N; i++) {
-				if(i%2==0) {
-					sb.append(que1.poll()).append(" ");
-				}else {
-					sb.append(que2.poll()).append(" ");
-				}
+
+			for (int i = 0; i < N; i++) {
+			    cards[i] = st.nextToken();
+			}
+
+			int mid = (N + 1) / 2;
+
+			for (int i = 0; i < N; i++) {
+			    if (i % 2 == 0) {
+			        sb.append(cards[i / 2]);
+			    } else {
+			        sb.append(cards[mid + i / 2]);
+			    }
+
+			    sb.append(" ");
 			}
 			System.out.println(sb);
 		}
