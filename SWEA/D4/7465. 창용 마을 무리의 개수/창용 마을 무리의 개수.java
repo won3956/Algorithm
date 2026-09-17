4c2e418c9e53980a7718eb4fac00e1bacecdaba9
JiwonMoon <132432108+won3956@users.noreply.github.com>
2026-09-17 16:01:20 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 102 ms, Memory: 28,288 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -1,47 +1,45 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
-	static int[] parent;
+	static int N, M;
+	static int[] parents;
+	static int result;
 	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		int T = Integer.parseInt(br.readLine());
 		for (int test_case = 1; test_case <= T; test_case++) {
 			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int M = Integer.parseInt(st.nextToken());
-			parent = new int[N+1];
-			for(int i = 1; i <= N; i++) {
-				parent[i]=i;
-			}
-			for(int i = 0; i < M; i++) {
-				st = new StringTokenizer(br.readLine());
-				int x = Integer.parseInt(st.nextToken());
-				int y = Integer.parseInt(st.nextToken());
-				union(x, y);
-			}
-			int count = 0;
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			parents = new int[N+1];
+			result = N;
 			for (int i = 1; i <= N; i++) {
-				if (parent[i] == i) {
-					count++;
-				}
+				parents[i] = i;
 			}
-			System.out.println("#"+test_case+" "+count);
+			for (int i = 0; i < M; i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				union(a, b);
+			}
+			
+			
+			System.out.println("#"+test_case+" "+result);
 		}
 	}
 	public static void union(int a, int b) {
-		int rootA = find(a);
-		int rootB = find(b);
+		a = find(a);
+		b = find(b);
 		
-		if(rootA != rootB) {
-			parent[rootB] = rootA;
-		}
+		if(a == b) return;
+		
+		parents[a] = b;
+		result--;
 	}
 	public static int find(int a) {
-		if(parent[a]==a)	return a;
-		return parent[a] = find(parent[a]);
+		if(a == parents[a]) return a;
+		return parents[a] = find(parents[a]);
 	}
 }
