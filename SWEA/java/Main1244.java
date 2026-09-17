f79e01ace71afd71b4e1ce15d1719a5dd86f0106
JiwonMoon <132432108+won3956@users.noreply.github.com>
2026-09-17 12:01:20 +0900

    Delete SWEA/java directory delete

--- SWEA/java/Main1244.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-import java.util.*;
-
-public class Main1244 {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-    }
-}
